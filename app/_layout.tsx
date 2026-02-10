64a7874f3ca3b613e1880a1719825db4714818ec
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2026-02-10 16:31:42 -0500

    fix: routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(main)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
