4b79fe0f2bc6c8281d78b55d7f86b5e9853f2f55
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2026-02-25 19:12:21 -0500

    chore: set dev config

--- constants/DevConfig.ts
+++ constants/DevConfig.ts
@@ -1,6 +1,6 @@
 export const DevConfig = {
     // Set to true to enable the initial route override
-    enableDevRouting: true,
+    enableDevRouting: false,
 
     initialRoute: '/(main)/bookInfo',
 };
